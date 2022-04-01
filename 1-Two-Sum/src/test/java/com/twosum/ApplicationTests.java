d84768be982505588b9fb8207bc9d3dd9db5d6ab
Minjae Kim <minjae.kim@mytrum.com>
2022-04-01 20:53:48 +0900

    1. Two Sum - {4}

---
+++ 1-Two-Sum/src/test/java/com/twosum/ApplicationTests.java
@@ -0,0 +1,60 @@
+package com.twosum;
+
+import com.twosum.solution.TwoSum;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void twoSumTest() {
+
+        //given:
+        int[] nums = new int[5]; // nums = [5,4,8,1,9]
+        nums[0] = 5;
+        nums[1] = 4;
+        nums[2] = 8;
+        nums[3] = 1;
+        nums[4] = 9;
+
+        int target = 17;
+
+        int[] resultNums = new int[2];
+        resultNums[0] = 4; // 9
+        resultNums[1] = 2; // 8
+
+        //when:
+        int[] result = TwoSum.twoSum(nums, target);
+
+        //then:
+        assert result[0] == resultNums[0];
+        assert result[1] == resultNums[1];
+    }
+
+    @Test
+    void twoSumNegativeValueTest() {
+
+        //given:
+        int[] nums = new int[5]; // nums = [5,4,8,1,9]
+        nums[0] = -5;
+        nums[1] = -4;
+        nums[2] = -8;
+        nums[3] = -1;
+        nums[4] = -9;
+
+        int target = -17;
+
+        int[] resultNums = new int[2];
+        resultNums[0] = 4; // 9
+        resultNums[1] = 2; // 8
+
+        //when:
+        int[] result = TwoSum.twoSum(nums, target);
+
+        //then:
+        assert result[0] == resultNums[0];
+        assert result[1] == resultNums[1];
+    }
+
+}

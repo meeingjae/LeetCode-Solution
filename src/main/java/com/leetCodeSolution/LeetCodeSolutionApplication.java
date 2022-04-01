e5fee82a49827c239554b097bae85c2c608252fb
Minjae Kim <minjae.kim@mytrum.com>
2022-04-01 17:09:25 +0900

    Initial commit

---
+++ src/main/java/com/leetCodeSolution/LeetCodeSolutionApplication.java
@@ -0,0 +1,14 @@
+package com.leetCodeSolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetCodeSolutionApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(LeetCodeSolutionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leetCodeSolution/LeetCodeSolutionApplicationTests.java
@@ -0,0 +1,14 @@
+package com.leetCodeSolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCodeSolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}

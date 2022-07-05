0e4a1c38bc30fcb8752ec47138d6cf0f2ac14de9
Minjae Kim <minjae.kim@mytrum.com>
2022-07-05 22:07:47 +0900

    feat: 152. Maximum Product Subarray 생성

---
+++ src/main/java/com/leetCodeSolution/solutions150to200/solutions152/MaximumProductSubarray.java
@@ -0,0 +1,8 @@
+package com.leetCodeSolution.solutions150to200.solutions152;
+
+public class MaximumProductSubarray {
+
+    public static int maxProduct(int[] nums) {
+        return 1;
+    }
+}
---
+++ src/test/java/com/leetCodeSolution/solutions150to200/solutions152/MaximumProductSubarrayTest.java
@@ -0,0 +1,13 @@
+package com.leetCodeSolution.solutions150to200.solutions152;
+
+import org.junit.jupiter.api.Test;
+
+public class MaximumProductSubarrayTest {
+
+    @Test
+    void 성공() {
+        //given:
+        //when:
+        //then:
+    }
+}

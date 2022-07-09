149469918760882aa21d7ccf753f640352122476
Minjae Kim <minjae.kim@mytrum.com>
2022-07-09 09:22:25 +0900

    feat: 155. Min Stack 구현

--- src/test/java/com/leetCodeSolution/solutions150to200/solutions155/MinStackTest.java
+++ src/test/java/com/leetCodeSolution/solutions150to200/solutions155/MinStackTest.java
@@ -1,5 +1,7 @@
 package com.leetCodeSolution.solutions150to200.solutions155;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 
 public class MinStackTest {
@@ -7,7 +9,18 @@ public class MinStackTest {
     @Test
     void 성공() {
         //given:
+        MinStack stack = new MinStack();
         //when:
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        minStack.getMin(); // return -3
+        minStack.pop();
+        minStack.top();    // return 0
+        minStack.getMin(); // return -2
         //then:
+        assertThat(minStack.top()).isEqualTo(0);
+        assertThat(minStack.getMin()).isEqualTo(-2);
     }
 }

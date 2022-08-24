8a412415b689359c646017fac9e82f87d8297ee7
Minjae Kim <minjae.kim@mytrum.com>
2022-08-24 18:24:42 +0900

    feat: 201. Bitwise And Number Range 구현

--- src/main/java/com/leetCodeSolution/solutions200to250/solutions201/BitwiseAndNumRange.java
+++ src/main/java/com/leetCodeSolution/solutions200to250/solutions201/BitwiseAndNumRange.java
@@ -1,7 +1,26 @@
 package com.leetCodeSolution.solutions200to250.solutions201;
 
 public class BitwiseAndNumRange {
+    /**
+     * 10 - 01010
+     * 11 - 01011
+     * 12 - 01100
+     * 13 - 01001
+     * 14 - 01010
+     * 15 - 01100
+     * 16 - 01101
+     * 17 - 01110
+     * 18 - 01111
+     * 19 - 10000
+     * 20 - 10001
+     */
     public static int rangeBitwiseAnd(int left, int right) {
-        return 0;
+        int i = 0;
+        while(left != right){
+            left >>= 1;
+            right >>= 1;
+            i++;
+        }
+        return left << i;
     }
 }
--- src/test/java/com/leetCodeSolution/solutions200to250/solutions201/BitwiseAndNumRangeTest.java
+++ src/test/java/com/leetCodeSolution/solutions200to250/solutions201/BitwiseAndNumRangeTest.java
@@ -15,4 +15,24 @@ public class BitwiseAndNumRangeTest {
         //then:
         assertThat(result).isEqualTo(4);
     }
+
+    @Test
+    void zeroTest() {
+        //given:
+        int left = 0, right = 0;
+        //when:
+        int result = BitwiseAndNumRange.rangeBitwiseAnd(left,right);
+        //then:
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    void maxTest() {
+        //given:
+        int left = 1, right = 2147483647;
+        //when:
+        int result = BitwiseAndNumRange.rangeBitwiseAnd(left,right);
+        //then:
+        assertThat(result).isEqualTo(0);
+    }
 }

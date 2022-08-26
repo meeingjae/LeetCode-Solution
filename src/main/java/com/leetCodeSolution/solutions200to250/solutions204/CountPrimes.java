c121a340fe4ac3828a4afa45375ddf2b6c2e7256
Minjae Kim <minjae.kim@mytrum.com>
2022-08-26 14:38:14 +0900

    feat: 204 - Cout Primes 구현

--- src/main/java/com/leetCodeSolution/solutions200to250/solutions204/CountPrimes.java
+++ src/main/java/com/leetCodeSolution/solutions200to250/solutions204/CountPrimes.java
@@ -2,6 +2,16 @@ package com.leetCodeSolution.solutions200to250.solutions204;
 
 public class CountPrimes {
     public static int countPrimes(int n) {
-        return 0;
+        boolean[] checkedPrime = new boolean[n+1];
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (!checkedPrime[i]) {
+                count++;
+            }
+            for (int j = i; j <= n; j += i) {
+                checkedPrime[j] = true;
+            }
+        }
+        return count;
     }
 }

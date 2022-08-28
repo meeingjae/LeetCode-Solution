224d545d3e2b9bf981e3335b821f67ba27d68000
Minjae Kim <minjae.kim@mytrum.com>
2022-08-28 09:37:17 +0900

    build: 207 - Course Schedule

---
+++ src/main/java/com/leetCodeSolution/solutions200to250/solutions207/CourseSchedule.java
@@ -0,0 +1,7 @@
+package com.leetCodeSolution.solutions200to250.solutions207;
+
+public class CourseSchedule {
+    public static boolean cource(int numCourses, int[][] prerequisites) {
+        return true;
+    }
+}

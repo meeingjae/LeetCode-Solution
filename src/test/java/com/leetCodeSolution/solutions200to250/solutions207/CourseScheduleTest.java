f1095db82639e810cb6640855975a0d1d497867d
Minjae Kim <minjae.kim@mytrum.com>
2022-08-30 18:08:11 +0900

    build: 207 - Course Schedule 구현 (BFS)

--- src/test/java/com/leetCodeSolution/solutions200to250/solutions207/CourseScheduleTest.java
+++ src/test/java/com/leetCodeSolution/solutions200to250/solutions207/CourseScheduleTest.java
@@ -7,34 +7,56 @@ import org.junit.jupiter.api.Test;
 public class CourseScheduleTest {
 
     @Test
-    void 성공() {
+    void 성공_DFS() {
         //given:
         int numCources = 2;
         int[][] prerequisites = { { 1, 0 } };
         //when:
-        boolean result = CourseSchedule.cource(numCources, prerequisites);
+        boolean result = CourseSchedule.courseDfs(numCources, prerequisites);
         //then:
         assertThat(result).isTrue();
     }
 
     @Test
-    void 성공_2() {
+    void 성공_DFS_2() {
         //given:
         int numCources = 2;
         int[][] prerequisites = { { 1, 0 }, { 0, 1 } };
         //when:
-        boolean result = CourseSchedule.cource(numCources, prerequisites);
+        boolean result = CourseSchedule.courseDfs(numCources, prerequisites);
         //then:
         assertThat(result).isFalse();
     }
 
     @Test
-    void 실패() {
+    void 실패_DFS() {
         //given:
         int numCources = 4;
         int[][] prerequisites = { { 3, 2 }, { 2, 1 }, { 1, 0 }, { 0, 3 }, { 3, 1 }, { 0, 2 } };
         //when:
-        boolean result = CourseSchedule.cource(numCources, prerequisites);
+        boolean result = CourseSchedule.courseDfs(numCources, prerequisites);
+        //then:
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void 성공_BFS() {
+        //given:
+        int numCources = 2;
+        int[][] prerequisites = { { 1, 0 } };
+        //when:
+        boolean result = CourseSchedule.courseBfs(numCources, prerequisites);
+        //then:
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void 실패_BFS() {
+        //given:
+        int numCources = 4;
+        int[][] prerequisites = { { 3, 2 }, { 2, 1 }, { 1, 0 }, { 0, 3 }, { 3, 1 }, { 0, 2 } };
+        //when:
+        boolean result = CourseSchedule.courseDfs(numCources, prerequisites);
         //then:
         assertThat(result).isFalse();
     }

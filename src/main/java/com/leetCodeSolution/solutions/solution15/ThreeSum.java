c3c9708c3b358b6e56238dd62efa02d3e0f8d8f9
Minjae Kim <minjae.kim@mytrum.com>
2022-09-10 17:19:21 +0900

    build: 15 - 3Sum 프로젝트 생성

---
+++ src/main/java/com/leetCodeSolution/solutions/solution15/ThreeSum.java
@@ -0,0 +1,15 @@
+package com.leetCodeSolution.solutions.solution15;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ThreeSum {
+    public ThreeSum(int[] input) {
+
+    }
+
+    public List<List<Integer>> sum() {
+
+        return Collections.emptyList();
+    }
+}
---
+++ src/test/java/com/leetCodeSolution/solutions/solution15/ThreeSumTest.java
@@ -0,0 +1,25 @@
+package com.leetCodeSolution.solutions.solution15;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class ThreeSumTest {
+
+    @Test
+    void 성공() {
+        //given:
+        int[] input = { -1, 0, 1, 2, -1, -4 };
+        //when:
+        ThreeSum threeSum = new ThreeSum(input);
+        List<List<Integer>> result = threeSum.sum();
+        //then:
+        assertThat(result).hasSize(2);
+        assertThat(result.get(0)).hasSize(3);
+        assertThat(result.get(1)).hasSize(3);
+        assertThat(result.get(0)).containsSequence(-1, -1, 2);
+        assertThat(result.get(1)).containsSequence(-1, 0, 1);
+    }
+}

df73022e8a6718c17ec2b970e171fc1276cec3bd
Minjae Kim <minjae.kim@mytrum.com>
2022-10-11 17:07:17 +0900

    test: Test Case 추가

---
+++ src/main/java/com/leetCodeSolution/solutions/solution17/LetterCombinationsPhoneNumber.java
@@ -0,0 +1,10 @@
+package com.leetCodeSolution.solutions.solution17;
+
+import java.util.Collections;
+import java.util.List;
+
+public class LetterCombinationsPhoneNumber {
+    public static List<String> letterCombinations(String input) {
+        return Collections.emptyList();
+    }
+}
--- src/test/java/com/leetCodeSolution/solutions/solution17/LetterCombinationsPhoneNumberTest.java
+++ src/test/java/com/leetCodeSolution/solutions/solution17/LetterCombinationsPhoneNumberTest.java
@@ -1,5 +1,20 @@
-package test.java.com.leetCodeSolution.solutions.solution17;
+package com.leetCodeSolution.solutions.solution17;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
 
 public class LetterCombinationsPhoneNumberTest {
 
+    @Test
+    void name() {
+        String input = "23";
+        List<String> result = LetterCombinationsPhoneNumber.letterCombinations(input);
+        assertThat(result).containsSequence(
+                Arrays.asList("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf")
+        );
+    }
 }

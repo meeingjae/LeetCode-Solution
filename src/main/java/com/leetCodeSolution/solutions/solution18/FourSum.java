3e8ab769d3a1eebb582d395e1f022d64151da55c
Minjae Kim <minjae.kim@mytrum.com>
2022-10-14 17:26:26 +0900

    feat: 18 - 4Sum 생성

---
+++ src/main/java/com/leetCodeSolution/solutions/solution18/FourSum.java
@@ -0,0 +1,10 @@
+package com.leetCodeSolution.solutions.solution18;
+
+import java.util.Collections;
+import java.util.List;
+
+public class FourSum {
+    public static List<List<Integer>> sum(int[] nums, int target) {
+        return Collections.emptyList();
+    }
+}

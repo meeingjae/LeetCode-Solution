398e9c205ca0ba7dfbdac994cbf87224e0833a66
Minjae Kim <minjae.kim@mytrum.com>
2022-10-16 13:13:28 +0900

    feat: 142 - linked list cycle ii 구현
    ```
    * walker , runner 패턴 사용
    (1) walker와 runner에 head 할당
    (2) walker와 runner가 만날때까지 반복 (walker = 1칸씩, runner는 2칸씩 이동)
    (2-1) runner와 runner.next가 null이면 cycle은 없다. 따라서 null 반환
    (3) walker와 runner가 만나는 지점 (Cycle이 확인되는 지점)을 발견하면, runner에 head를 할당하고 다시 cycle 돌린다.
    (3-1) (A) runner가 head 부터 cycle 시작 지점 'P'까지 도착하는 거리
    (3-2) (B) walker가 이전에 cycle의 존재가 확인 된 순간으로부터 다시 cycle 시작 지점 'P'까지 도착하는 거리
    (3-3) (A) == (B)
    (4) 따라서 (3) 이후의 runner와 walker가 서로 만나는 지점이 Cycle 발생지점 'P'이다
    ```

--- src/main/java/com/leetCodeSolution/solutions100to150/solution142/LinkedListCycleII.java
+++ src/main/java/com/leetCodeSolution/solutions100to150/solution142/LinkedListCycleII.java
@@ -1,28 +1,35 @@
 package com.leetCodeSolution.solutions100to150.solution142;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import com.leetCodeSolution.common.ListNode;
 
 public class LinkedListCycleII {
     public static ListNode hasCycle(ListNode head) {
-        if (head == null || head.next == null) {
-            return null;
-        }
-        Set<Integer> valueSet = new HashSet<>();
-        valueSet.add(head.val);
-        return hasCycle(head.next, valueSet);
-    }
+        // walker와 runner에 Assign
+        ListNode walker = head;
+        ListNode runner = head;
 
-    public static ListNode hasCycle(ListNode node, Set<Integer> set) {
-        if (set.contains(node.val)) {
-            return node;
-        } else if (node.next != null) {
-            set.add(node.val);
-            return hasCycle(node.next, set);
-        } else {
-            return null;
+        // ruuner와 runner의 next가 null이 아닌 경우 Cycle 존재
+        while (runner != null && runner.next != null) {
+            // walker = 1칸, runner = 2칸씩 이동
+            walker = walker.next;
+            runner = runner.next.next;
+
+            //walker와 runner가 만나는 지점 (Cycle의 존재가 확인되는 순간 (어디가 cycle인지는 모름))
+            if (walker == runner) {
+                // cycle을 진행중이던 walker 혹은 runner 아무에게나 최초 head 할당
+                // 새로운 sloe2 라는 Node를 생성해도 무방하다
+                runner = head;
+                // cycle을 진행중이던 Node와 새로 head를 할당받은 node가 다시 순환
+                // (A) runner가 head 부터 cycle 시작 지점 'P'까지 도착하는 거리
+                // (B) walker가 이전에 cycle의 존재가 확인 된 순간으로부터 다시 cycle 시작 지점 'P'까지 도착하는 거리
+                // A == B
+                while (runner != walker) {
+                    runner = runner.next;
+                    walker = walker.next;
+                }
+                return walker;
+            }
         }
+        return null;
     }
 }

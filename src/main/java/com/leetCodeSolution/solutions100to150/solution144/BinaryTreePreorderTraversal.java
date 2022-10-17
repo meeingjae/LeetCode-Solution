5082d39294cc6285240fe6c13fa6af1221969712
Minjae Kim <minjae.kim@mytrum.com>
2022-10-18 08:36:42 +0900

    feat: 144 - binary tree preorder travelsal 구현
    * 이진트리 탐색
    * 너비 우선 탐색(BFS)

--- src/main/java/com/leetCodeSolution/solutions100to150/solution144/BinaryTreePreorderTraversal.java
+++ src/main/java/com/leetCodeSolution/solutions100to150/solution144/BinaryTreePreorderTraversal.java
@@ -1,12 +1,23 @@
 package com.leetCodeSolution.solutions100to150.solution144;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.leetCodeSolution.common.TreeNode;
 
 public class BinaryTreePreorderTraversal {
     public static List<Integer> preorderTraversal(TreeNode root) {
-        return Collections.emptyList();
+        List<Integer> res = new ArrayList<>();
+        travel(root, res);
+        return res;
+    }
+
+    private static void travel(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        res.add(root.val);
+        travel(root.left, res);
+        travel(root.right, res);
     }
 }

3b2299b50fbb549f601ee1f4434c791a82e67d24
Minjae Kim <minjae.kim@mytrum.com>
2022-10-22 08:35:34 +0900

    build: 168 excel sheet column title 생성

---
+++ src/test/java/com/leetCodeSolution/solutions150to200/solutions168/ExcelSheetColumnTitleTest.java
@@ -0,0 +1,38 @@
+package com.leetCodeSolution.solutions150to200.solutions168;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class ExcelSheetColumnTitleTest {
+
+    @Test
+    void example1() {
+        //given:
+        int columnNumber = 1;
+        //when:
+        String res = ExcelSheetColumnTitle.convertToTitle(columnNumber);
+        //then:
+        assertThat(res).isEqualTo("A");
+    }
+
+    @Test
+    void example2() {
+        //given:
+        int columnNumber = 28;
+        //when:
+        String res = ExcelSheetColumnTitle.convertToTitle(columnNumber);
+        //then:
+        assertThat(res).isEqualTo("AB");
+    }
+
+    @Test
+    void example3() {
+        //given:
+        int columnNumber = 701;
+        //when:
+        String res = ExcelSheetColumnTitle.convertToTitle(columnNumber);
+        //then:
+        assertThat(res).isEqualTo("ZY");
+    }
+}

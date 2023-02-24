52f45a38d0e01ec05db1b423fddc3400a716884c
Minjae Kim <minjae.kim@mytrum.com>
2023-02-24 21:25:38 +0900

    docs: 338. counting bits

---
+++ src/main/java/com/leetCodeSolution/dp/solution338/CountingBits.java
@@ -0,0 +1,8 @@
+package com.leetCodeSolution.dp.solution338;
+
+public class CountingBits {
+    public static int[] countBits(int n) {
+
+        return new int[0];
+    }
+}
---
+++ src/test/java/com/leetCodeSolution/dp/solution338/CountingBitsTest.java
@@ -0,0 +1,30 @@
+package com.leetCodeSolution.dp.solution338;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class CountingBitsTest {
+
+    @Test
+    void test1() {
+        //given:
+        final int n = 2;
+        final int[] expected = { 0, 1, 2 };
+        //when:
+        final int[] result = CountingBits.countBits(n);
+        //then:
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void test2() {
+        //given:
+        final int n = 5;
+        final int[] expected = { 0, 1, 1, 2, 1, 2 };
+        //when:
+        final int[] result = CountingBits.countBits(n);
+        //then:
+        assertThat(result).isEqualTo(expected);
+    }
+}

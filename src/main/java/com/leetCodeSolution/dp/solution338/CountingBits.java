ec2d322b39ee86c3e175533fa56a76c01284001a
Minjae Kim <minjae.kim@mytrum.com>
2023-02-25 09:33:47 +0900

    feat: 338. counting bits

--- src/main/java/com/leetCodeSolution/dp/solution338/CountingBits.java
+++ src/main/java/com/leetCodeSolution/dp/solution338/CountingBits.java
@@ -2,7 +2,39 @@ package com.leetCodeSolution.dp.solution338;
 
 public class CountingBits {
     public static int[] countBits(int n) {
+        int[] result = new int[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            result[i] = solve(i);
+        }
+        return result;
+    }
 
-        return new int[0];
+    private static int solve(int i) {
+        if (i == 0) {
+            return 0;
+        }
+        if (i == 1) {
+            return 1;
+        }
+        if (i % 2 == 0) { //even
+            return solve(i / 2);
+        } else {
+            return solve(i / 2) + 1;
+        }
+    }
+
+    public static int[] countBits2(int n) {
+        int[] result = new int[n + 1];
+        if (n == 0) {
+            return result;
+        }
+        for (int i = 0; i < n + 1; i++) {
+            if (i % 2 == 0) {
+                result[i] = result[i / 2];
+                continue;
+            }
+            result[i] = result[i - 1] + 1;
+        }
+        return result;
     }
 }
--- src/test/java/com/leetCodeSolution/dp/solution338/CountingBitsTest.java
+++ src/test/java/com/leetCodeSolution/dp/solution338/CountingBitsTest.java
@@ -10,7 +10,7 @@ public class CountingBitsTest {
     void test1() {
         //given:
         final int n = 2;
-        final int[] expected = { 0, 1, 2 };
+        final int[] expected = { 0, 1, 1 };
         //when:
         final int[] result = CountingBits.countBits(n);
         //then:
@@ -24,6 +24,61 @@ public class CountingBitsTest {
         final int[] expected = { 0, 1, 1, 2, 1, 2 };
         //when:
         final int[] result = CountingBits.countBits(n);
+        //then:
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void test3() {
+        //given:
+        final int n = 15;
+        final int[] expected = { 0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4 };
+        //when:
+        final int[] result = CountingBits.countBits(n);
+        //then:
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void test4() {
+        //given:
+        final int n = 2;
+        final int[] expected = { 0, 1, 1 };
+        //when:
+        final int[] result = CountingBits.countBits2(n);
+        //then:
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void test5() {
+        //given:
+        final int n = 5;
+        final int[] expected = { 0, 1, 1, 2, 1, 2 };
+        //when:
+        final int[] result = CountingBits.countBits2(n);
+        //then:
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void test6() {
+        //given:
+        final int n = 15;
+        final int[] expected = { 0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4 };
+        //when:
+        final int[] result = CountingBits.countBits2(n);
+        //then:
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void test7() {
+        //given:
+        final int n = 0;
+        final int[] expected = { 0 };
+        //when:
+        final int[] result = CountingBits.countBits2(n);
         //then:
         assertThat(result).isEqualTo(expected);
     }

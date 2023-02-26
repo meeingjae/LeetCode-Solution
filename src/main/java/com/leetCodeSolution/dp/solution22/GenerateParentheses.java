94181864bf2656c2675cc8e07184f65e6909f180
Minjae Kim <minjae.kim@mytrum.com>
2023-02-26 21:40:34 +0900

    docs: 22. generate parentheses

---
+++ src/main/java/com/leetCodeSolution/dp/solution22/GenerateParentheses.java
@@ -0,0 +1,9 @@
+package com.leetCodeSolution.dp.solution22;
+
+import java.util.List;
+
+public class GenerateParentheses {
+    public static List<String> generateParenthesis(int n) {
+        return null;
+    }
+}

6420347a9928f1fc3d81ca00100b411ead6299ed
Minjae Kim <minjae.kim@mytrum.com>
2023-02-27 20:57:23 +0900

    feat: 22. generate parentheses

--- src/main/java/com/leetCodeSolution/dp/solution22/GenerateParentheses.java
+++ src/main/java/com/leetCodeSolution/dp/solution22/GenerateParentheses.java
@@ -1,9 +1,25 @@
 package com.leetCodeSolution.dp.solution22;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GenerateParentheses {
     public static List<String> generateParenthesis(int n) {
-        return null;
+        List<String> result = new ArrayList<>();
+        make(0, 0, result, "", n);
+        return result;
+    }
+
+    private static void make(int open, int close, List<String> result, String s, int n) {
+        if (s.length() == n * 2) {
+            result.add(s);
+            return;
+        }
+        if (open < n) {
+            make(open + 1, close, result, s + "(", n);
+        }
+        if (open > close) {
+            make(open, close + 1, result, s + ")", n);
+        }
     }
 }

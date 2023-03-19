13b3195223bfd1a2301502187935f7b578f70aee
Minjae Kim <minjae.kim@mytrum.com>
2023-03-19 20:43:05 +0900

    feat: 392. is subsequence

--- src/main/java/com/leetCodeSolution/dp/solution392/IsSubsequence.java
+++ src/main/java/com/leetCodeSolution/dp/solution392/IsSubsequence.java
@@ -2,6 +2,23 @@ package com.leetCodeSolution.dp.solution392;
 
 public class IsSubsequence {
     public static boolean isSubsequence(String s, String t) {
+        if(s.isBlank()) {
+            return true;
+        }
+        if(t.isBlank()) {
+            return false;
+        }
+        int sIndex = 0;
+        int tIndex = 0;
+        while(tIndex < t.length()) {
+            if(t.charAt(tIndex) == s.charAt(sIndex)) {
+                sIndex++;
+                if(sIndex == s.length()) {
+                    return true;
+                }
+            }
+            tIndex++;
+        }
         return false;
     }
 }

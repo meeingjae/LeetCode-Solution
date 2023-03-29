e6cababdd6100b4b3fa44307bfcad872ceda94d0
Minjae Kim <minjae.kim@mytrum.com>
2023-03-29 20:20:43 +0900

    feat: 746. min cost climbing stairs

--- src/main/java/com/leetCodeSolution/dp/solution746/MinCostClimbingStairs.java
+++ src/main/java/com/leetCodeSolution/dp/solution746/MinCostClimbingStairs.java
@@ -2,6 +2,14 @@ package com.leetCodeSolution.dp.solution746;
 
 public class MinCostClimbingStairs {
     public static int minCostClimbingStairs(int[] cost) {
-        return 0;
+        int current = 0;
+        int cost1 = 0;
+        int cost2 = 0;
+        for (int i = cost.length - 1; i >= 0; i--) {
+            current = cost[i] + Math.min(cost1, cost2);
+            cost2 = cost1;
+            cost1 = current;
+        }
+        return Math.min(cost1, cost2);
     }
 }
